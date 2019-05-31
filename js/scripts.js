abe1242581e38ed58520cde02c66777a8796d6d8
ElimuStuart <stuartelimu@gmail.com>
2019-05-31 22:35:01 +0300

    navbar now working

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 let MENU = document.querySelector(".menu")
+let OVERLAY = document.querySelector(".overlay")
 
 function toggleActive(){
     this.classList.toggle("active");
+    OVERLAY.classList.toggle("menu-open")
 }
 
 MENU.addEventListener("click", toggleActive);
